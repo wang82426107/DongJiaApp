deec42f8fa369e5bdb00360281fc8fcd6e0b83d9
骚栋 <676758285@qq.com>
2019-06-20 14:01:46 +0800

    start

---
+++ DongJia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DongJia
+//
+//  Created by bnqc on 2019/6/20.
+//  Copyright © 2019年 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DongJia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DongJia
+//
+//  Created by bnqc on 2019/6/20.
+//  Copyright © 2019年 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
